11c58d15a6997cff3df90dae9d4d5cd000dd57e7
AJEGUM <juliandguevara12@gmail.com>
2026-03-24 18:55:15 -0500

    Integracion de pagina principal

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path: '',
+    loadComponent: () => import('./pages/publico/inicio/inicio').then(m => m.Inicio),
+    title: 'SGO - Sistema de Gestion de OVAs'
+  },
+];
---
+++ abrirProyecto.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Abriendo proyecto en VS Code...
+
+cd /d C:\Users\Julian\Desktop\SGO
+code .
+
+exit
