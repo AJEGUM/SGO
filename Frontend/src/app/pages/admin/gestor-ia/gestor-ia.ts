cf798af23f2cdbd2c0c03f4ab81621286830bead
Julián Guevara <tu-correo@ejemplo.com>
2026-04-01 21:31:06 -0500

    Se integro la consulta de programa y competencias

--- Frontend/src/app/pages/admin/gestor-ia/gestor-ia.ts
+++ Frontend/src/app/pages/admin/gestor-ia/gestor-ia.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Admin, Competencia } from '../../../services/admin/admin';
 
 @Component({
   selector: 'app-gestor-ia',
-  imports: [],
-  templateUrl: './gestor-ia.html',
-  styleUrl: './gestor-ia.css',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './gestor-ia.html'
 })
-export class GestorIa {
+export class GestorIa implements OnInit {
+  programas: any[] = [];
+  competencias: Competencia[] = [];
+  
+  programaSeleccionadoId: number | null = null;
+  cargandoCompetencias: boolean = false;
 
+  constructor(private adminService: Admin) {}
+
+  ngOnInit(): void {
+    this.cargarProgramas();
+  }
+
+  cargarProgramas() {
+    this.adminService.getProgramas().subscribe(res => {
+      this.programas = res;
+    });
+  }
+
+  onProgramaChange() {
+    console.log('ID Seleccionado:', this.programaSeleccionadoId); // Si sale undefined, es el mapeo del HTML
+    if (this.programaSeleccionadoId) {
+      this.cargandoCompetencias = true;
+      this.competencias = []; // Limpiar lista actual
+      
+      this.adminService.getCompetencias(this.programaSeleccionadoId).subscribe({
+        next: (res) => {
+          this.competencias = res;
+          this.cargandoCompetencias = false;
+        },
+        error: () => this.cargandoCompetencias = false
+      });
+    }
+  }
+
+  generarTest(competencia: Competencia) {
+    console.log('Iniciando flujo de IA para:', competencia.nombre);
+    // Aquí abrirías tu modal de previsualización
+  }
 }
