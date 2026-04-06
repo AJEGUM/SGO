250aa2140df59db86902d6ff633163e05af426f5
AJEGUM <juliandguevara12@gmail.com>
2026-04-06 08:35:43 -0500

    fx

--- Frontend/src/app/pages/publico/beneficios/beneficios.ts
+++ Frontend/src/app/pages/publico/beneficios/beneficios.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Navbar } from '../../../components/publico/navbar/navbar';
 import { Footer } from '../../../components/publico/footer/footer';
 
