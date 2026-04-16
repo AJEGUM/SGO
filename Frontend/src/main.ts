3a78f054e19d6cc7c6aa8685d14eb463d5bc99e1
AJEGUM <juliandguevara12@gmail.com>
2026-04-16 17:31:24 -0500

    volver a iniciar

--- Frontend/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app';
-import { appConfig } from './app/app.config'; // Importamos tu configuración con el interceptor
-
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
