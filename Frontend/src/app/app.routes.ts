9e6022ec4872e29bede3ecf4e6ebe4f02cfb329d
AJEGUM <juliandguevara12@gmail.com>
2026-04-16 18:09:10 -0500

    ruta base

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+//   {
+//     path: '',
+//     loadComponent: () => import('./pages/public/inicio/inicio').then(m => m.Inicio),
+//     title: 'Inicio - Perritos QR'
+//   },
+
+]
